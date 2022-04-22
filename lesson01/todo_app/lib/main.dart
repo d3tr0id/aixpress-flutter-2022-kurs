3dab8c94f58f66e7d03d12a83a0c8905f6682e25
Cem Kara <82kace1bil@hft-stuttgart.de>
2022-04-22 11:19:55 +0200

    Added basic tutorial comments for todo app

--- lesson01/todo_app/lib/main.dart
+++ lesson01/todo_app/lib/main.dart
@@ -30,24 +30,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // In diesen beiden Arrays sollen jeweils die Todos und Done String
+  // gespeichert werden
   List<String> todos = [];
   List<String> done = [];
+
+  // Dieser Controller wird benötigt für das TextField im AlertDialog
+  TextEditingController _textEditingController;
+
   @override
   void initState() {
     super.initState();
+    _textEditingController = TextEditingController();
   }
 
   @override
   void dispose() {
+    _textEditingController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    Future<String> _showCreateTodoDialog() {
-      return showDialog<String>(
+    _showCreateTodoDialog() {
+      // Der Alert Dialog soll ein TextField und zwei Buttons 'Cancel'
+      // und 'Create'enthalten
+      return showDialog(
           context: context,
           builder: (context) {
+            // Dialog braucht title, content und actions Parameter
+            // Sobald der Create Button geklickt wird soll
             return AlertDialog();
           });
     }
@@ -56,9 +68,11 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
+      // Hier brauchen wir einen body der in einer Column die Todos und Erledigten
+      // Aufgaben in Form von ListTile Widgets enthält
       floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        tooltip: 'Increment',
+        // onpressed Parameter soll Dialog aufrufen
+        tooltip: 'New Todo',
         child: const Icon(Icons.add),
       ),
     );
